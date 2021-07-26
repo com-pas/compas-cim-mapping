8b5c11c88a2bc20fd6dbeab2662870c470f49bdd
Dennis Labordus <dennis.labordus@alliander.com>
2021-07-26 14:39:34 +0200

    Use prefix configuration from CoMPAS core.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- app/src/main/java/org/lfenergy/compas/cim/mapping/rest/model/package-info.java
+++ app/src/main/java/org/lfenergy/compas/cim/mapping/rest/model/package-info.java
@@ -3,8 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 @XmlSchema(
         xmlns = {
-                @XmlNs(prefix = "cms", namespaceURI = CIM_MAPPING_SERVICE_NS_URI),
-                @XmlNs(prefix = "", namespaceURI = SCL_NS_URI)
+                @XmlNs(prefix = "cms", namespaceURI = CIM_MAPPING_SERVICE_NS_URI)
         }
 )
 package org.lfenergy.compas.cim.mapping.rest.model;
@@ -13,4 +12,3 @@ import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
 
 import static org.lfenergy.compas.cim.mapping.Constants.CIM_MAPPING_SERVICE_NS_URI;
-import static org.lfenergy.compas.cim.mapping.Constants.SCL_NS_URI;
