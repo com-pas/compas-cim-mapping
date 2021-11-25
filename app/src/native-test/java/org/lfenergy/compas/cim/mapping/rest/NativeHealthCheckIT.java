7c2ac40ceb7bd9cdf5d0525c6f299665bb52c96e
Dennis Labordus <dennis.labordus@alliander.com>
2021-11-25 11:43:49 +0100

    Updated Project settings and maven pom.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

---
+++ app/src/native-test/java/org/lfenergy/compas/cim/mapping/rest/NativeHealthCheckIT.java
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2021 Alliander N.V.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfenergy.compas.cim.mapping;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+class NativeHealthCheckIT extends HealthCheckTest {
+    // Execute the same tests but in native mode.
+}
