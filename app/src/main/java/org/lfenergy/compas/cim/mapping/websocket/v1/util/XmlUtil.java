5fb963dbcf7664d260e8f43a3e062e29e9fddafa
Nora Blomaard <n.blomaard@gmail.com>
2026-02-18 16:22:33 +0100

    feat: add WebSocket support for CIM mapping

---
+++ app/src/main/java/org/lfenergy/compas/cim/mapping/websocket/v1/util/XmlUtil.java
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2021 Alliander N.V.
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.compas.cim.mapping.websocket.v1.util;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class XmlUtil {
+    public static <T> T fromXml(String xml, Class<T> clazz) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        return (T) unmarshaller.unmarshal(new StringReader(xml));
+    }
+
+    public static <T> String toXml(T object) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(object.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(object, writer);
+        return writer.toString();
+    }
+}
